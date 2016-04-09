8d9e1343f5d3971ad10bf4b066043c0d39710978
Tung Dao <me@tungdao.com>
2016-04-10 06:27:05 +0700

    add scrolled content

--- src/main/javascript/analyzer.js
+++ src/main/javascript/analyzer.js
@@ -1,28 +1,56 @@
 
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import QueueAnim from 'rc-queue-anim'
+import ReactDOM from 'react-dom';
+import QueueAnim from 'rc-queue-anim';
+import _ from 'lodash';
 
 import Word from './word';
 
 const mock = [
-  // { text: 'Hello', duration: 10, final: true },
-  // { text: 'nice', duration: 10, final: true },
-  // { text: 'to', duration: 10, final: true },
-  // { text: 'meet', duration: 10, final: true },
-  // { text: 'you', duration: 10, final: true },
-  // { text: 'all', duration: 10, final: false },
+  { text: 'Hello', duration: 10, final: true },
+  { text: 'nice', duration: 10, final: true },
+  { text: 'to', duration: 10, final: true },
+  { text: 'meet', duration: 10, final: true },
+  { text: 'you', duration: 10, final: true },
+  { text: 'all', duration: 10, final: false },
 ]
+
+let scrolling = false;
+
+function scrollTo(element, to, duration) {
+  if (duration <= 0) return;
+  let difference = to - element.scrollTop,
+      perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+}
+
 
 export default class Analyzer extends Component {
 
   constructor(props) {
     super(props);
 
-    // this.state = { words: [] };
-    this.state = { words: mock }
+    this.props.recorder.onUpdate = words => this.setState({ words });
 
-    this.props.recorder.onUpdate = words => this.setState({ words })
+    // this.state = { words: mock };
+    this.state = { words: [] };
+
+    this.scrolling = false;
+    this.scrollToBottom = _.debounce(_ => {
+      if (this.scrolling) {
+        return;
+      }
+      ReactDOM.findDOMNode(this).scrollTop = 999999
+    });
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom()
   }
 
   renderWord(word, i) {
@@ -45,7 +73,9 @@ export default class Analyzer extends Component {
 
     return (
       <div className="analyzer">
-        {words.map(this.renderSentence.bind(this))}
+        <div className="analyzer__inner">
+          {words.map(this.renderSentence.bind(this))}
+        </div>
       </div>
     )
   }
