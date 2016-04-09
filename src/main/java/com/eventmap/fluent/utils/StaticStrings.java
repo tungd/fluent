56d577ba34e60a5cd3e40bbf7b534704804e3c54
Huy Tran <quanghuy.hut.fami@gmail.com>
2016-04-09 19:50:47 +0700

    add jaxb after refactor

---
+++ src/main/java/com/eventmap/fluent/utils/StaticStrings.java
@@ -0,0 +1,8 @@
+package com.eventmap.fluent.utils;
+
+/**
+ * Created by huytran on 4/9/16.
+ */
+public class StaticStrings {
+    public static final String RULES_PATH = "./src/main/resources/grammar.xml";
+}
--- src/main/java/com/eventmap/fluent/utils/XMLUtil.java
+++ src/main/java/com/eventmap/fluent/utils/XMLUtil.java
@@ -1,7 +1,26 @@
 package com.eventmap.fluent.utils;
 
+import com.eventmap.fluent.domain.jaxb.Rules;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 /**
  * Created by huytran on 4/9/16.
  */
 public class XMLUtil {
+
+    public static Rules UnMarshalRulesFile() throws JAXBException{
+        Rules rules;
+
+        File file = new File(StaticStrings.RULES_PATH);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Rules.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        rules = (Rules) jaxbUnmarshaller.unmarshal(file);
+
+        return rules;
+    }
+
 }
