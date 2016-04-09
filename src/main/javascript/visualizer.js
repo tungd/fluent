b1cd16bd9a9828f7f364d13c3f56a851cae7c51f
somallg <tq.duong@icloud.com>
2016-04-09 16:04:46 +0700

    Update

--- src/main/javascript/visualizer.js
+++ src/main/javascript/visualizer.js
@@ -1,9 +1,9 @@
+import React, { Component } from 'react';
+
 let requestAnimationFrame = (window.requestAnimationFrame ||
                              window.webkitRequestAnimationFrame ||
                              window.mozRequestAnimationFrame ||
-                             window.msRequestAnimationFrame).bind(window)
-
-import React, { Component } from 'react'
+                             window.msRequestAnimationFrame).bind(window);
 
 export default class Visualizer extends Component {
 
@@ -13,37 +13,39 @@ export default class Visualizer extends Component {
         width = canvas.width,
         height = canvas.height,
         graphic = canvas.getContext('2d'),
-        x = 0, y
+        x = 0, y;
+
+    console.log('canvas height', canvas.height);
 
     function draw() {
       if (x > width) {
-        x = 0
-        graphic.save()
-        graphic.fillStyle = '#fff'
-        graphic.fillRect(0, 0, width, height)
-        graphic.restore()
+        x = 0;
+        graphic.save();
+        graphic.fillStyle = '#fff';
+        graphic.fillRect(0, 0, width, height);
+        graphic.restore();
       }
 
-      y = height / 2 - recorder.analyze("energy") * 4
+      y = height / 2 - recorder.analyze("energy") * 4;
       if (x === 0) {
-        graphic.beginPath()
-        graphic.moveTo(x, y)
+        graphic.beginPath();
+        graphic.moveTo(x, y);
       } else {
-        graphic.lineTo(x, y)
+        graphic.lineTo(x, y);
       }
-      x += 1
+      x += 1;
 
-      graphic.strokeStyle = '#000'
-      graphic.strokeWidth = 2
-      graphic.stroke()
+      graphic.strokeStyle = '#000';
+      graphic.strokeWidth = 2;
+      graphic.stroke();
       requestAnimationFrame(draw)
     }
 
-    draw()
+    draw();
   }
 
   shouldComponentUpdate() {
-    return false
+    return false;
   }
 
   render() {
