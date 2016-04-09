c3445d055a228b23e84985f762b8726448c6a2a4
Tung Dao <me@tungdao.com>
2016-04-09 11:30:13 +0700

    Add basic layout

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 
+import Analyzer from './analyzer'
+import Visualizer from './visualizer'
+
+
 class App extends Component {
+
+  toggleRecording() {
+
+  }
+
   render() {
-    return <h1>Hello, World!!!</h1>
+    return (
+      <div className="app">
+        <Analyzer />
+        <div className="toolbar">
+          <button className="recording"
+                  onPress={this.toggleRecording.bind(this)}>
+            Recording
+          </button>
+          <Visualizer />
+        </div>
+      </div>
+    )
   }
 }
 
---
+++ src/main/javascript/visualizer.js
@@ -0,0 +1,9 @@
+
+import React, { Component } from 'react'
+
+export default class Visualizer extends Component {
+
+  render() {
+    return <div>{"Visualizer"}</div>
+  }
+}
